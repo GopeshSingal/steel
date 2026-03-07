47d96975533b9f817e77a0f1ecdfc3bd7f6795bc
GopeshSingal <gopesh.singal.03@gmail.com>
2026-03-06 23:30:28 -0500

    Implement metadata parsing using the ``tag`` package - Audio files have metadata embedded, but parsing it directly can be difficult - The ``tag`` package can perform the metadata parsing for us - If we require more advanced information in the future, we can write it ourselves

--- backend/go.mod
+++ backend/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/gorilla/sessions v1.2.2
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/dhowden/tag v0.0.0-20240417053706-3d75831295e8
 )
---
+++ backend/internal/library/metadata.go
@@ -0,0 +1,45 @@
+package library
+
+import (
+	"os"
+
+	"github.com/dhowden/tag"
+)
+
+type TrackMeta struct {
+	Title string
+	Artist string
+	Album string
+	Year int
+	TrackNo int
+	DiscNo int
+}
+
+func ParseMeta(path string) (TrackMeta, os.FileInfo, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return TrackMeta{}, nil, err
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return TrackMeta{}, nil, err
+	}
+
+	m, err := tag.ReadFrom(f)
+	if err != nil {
+		// Also returning FileInfo to try to scan without metadata
+		return TrackMeta{}, fi, nil
+	}
+
+	meta := TrackMeta{
+		Title: m.Title(),
+		Artist: m.Artist(),
+		Album: m.Album(),
+		Year: m.Year(),
+		TrackNo: m.Track(),
+		DiscNo: m.Disc(),
+	}
+	return meta, fi, nil
+}
